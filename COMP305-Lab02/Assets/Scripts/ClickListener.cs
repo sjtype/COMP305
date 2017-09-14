535ac1134df6546b9c1e1556c18b27e360b8e8af
sjtype <31806705+sjtype@users.noreply.github.com>
2017-09-14 18:40:00 -0400

    [ADD] Restarted Lab 2 in COMP305-Lab02
    I think I broke a file (UnityLockFile?), so I remade the project in another folder.

---
+++ COMP305-Lab02/Assets/Scripts/ClickListener.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickListener : MonoBehaviour {
+
+    //Sprites for different shirts
+    public Sprite blackShirt;
+    public Sprite redShirt;
+    public Sprite blueShirt;
+
+    //GameObject = pixel me
+    public GameObject person;
+
+    //SpriteRenderer is a component that renders the sprite...
+    SpriteRenderer rend;
+
+    void Start()
+    {
+        //first get the SpriteRenderer of the pixel person
+        rend = person.GetComponent<SpriteRenderer>();
+    }
+
+    //method to change sprite of the pixel person (different shirt colours)
+    void OnMouseDown()
+    {
+        rend.sprite = redShirt;
+    }
+}
