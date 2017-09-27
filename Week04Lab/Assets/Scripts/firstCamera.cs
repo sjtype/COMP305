ef812dc5496e589d1cfb14283c5f62d2eb584d62
sjtype <31806705+sjtype@users.noreply.github.com>
2017-09-27 18:57:30 -0400

    [ADD] Week 04 Lab
    Completed the lab as per the guidelines and built a WebGL solution
    - focus on camera behaviour (and some player movement)

---
+++ Week04Lab/Assets/Scripts/firstCamera.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstCamera : MonoBehaviour {
+
+    public Transform player;
+    //don't use GameObject because it'll take all of its components
+
+    //refers to Transform component of camera
+    private Transform camTrans;
+    //private Camera camCam;
+
+    // Use this for initialization
+    void Start()
+    {
+        camTrans = this.transform;
+        //shortcut for camTrans = this.GetComponent<Transform>(); (available since Transform is a common component)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.position.x <= -3.2)
+        {
+            camTrans.position = new Vector3(player.position.x, player.position.y, camTrans.position.z);
+        }
+
+        else if (player.position.x > -3.2 && player.position.x <= 3.2)
+        {
+            camTrans.position = new Vector3(0, 0, camTrans.position.z);
+            Camera.main.orthographicSize = 1.85f;
+        }
+
+        else if (player.position.x > 3.2)
+        {
+            camTrans.position = new Vector3(6.37f, 0, camTrans.position.z);
+
+            if (player.gameObject.GetComponent<Rigidbody2D>().velocity.x > 0)
+            {
+                Camera.main.orthographicSize -= 0.01f;
+            }
+
+            if (player.gameObject.GetComponent<Rigidbody2D>().velocity.x < 0)
+            {
+                Camera.main.orthographicSize += 0.01f;
+            }
+        }
+    }
+}
---
+++ Week04Lab/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax;
+}
+
+public class playerMovement : MonoBehaviour {
+
+    //public float is now available in unity
+    public float speed;
+    public Boundary boundary;
+
+    private Rigidbody2D rBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        //cached to save this -> establish connection to RigidBody2D
+        rBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Use this for physics
+    void FixedUpdate()
+    {
+        float horizMove = Input.GetAxis("Horizontal");
+
+        //to move character along 2 axis
+        Vector3 movement = new Vector3(horizMove, 0, 0);
+
+        //to alter speed of movement
+        rBody.velocity = movement * speed;
+        rBody.position = new Vector3(Mathf.Clamp(rBody.position.x, boundary.xMin, boundary.xMax), 0, 0);
+    }
+}
