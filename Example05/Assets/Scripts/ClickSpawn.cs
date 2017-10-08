6f8606fd98074a6eda5232c97e74d43dc6b3cee8
sjtype <31806705+sjtype@users.noreply.github.com>
2017-10-08 14:37:49 -0400

    [ADD] Week 05 Lab
    Spawn on click and timed explosion

---
+++ Example05/Assets/Scripts/ClickSpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSpawn : MonoBehaviour {
+
+    public GameObject bomb;
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            print(Input.mousePosition);
+            Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+            print(p);
+            print(p.x);
+            print(p.y);
+
+            Instantiate(bomb, new Vector3(p.x, p.y, 0.0f), Quaternion.identity);
+
+        }
+    }
+}
